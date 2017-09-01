6604d055a9fd80499261e28297fe5917bae49845
Mark Albright <kalani729@gmail.com>
2017-09-01 09:29:21 -0600

    Notes on namespaces and this

---
+++ 026_namespaces.ts
@@ -0,0 +1,83 @@
+// It protects the methods so that you are able to use the same method names without issues.
+
+
+[10:28] 
+In which you just specify then the name of the namespace followed by the method name.
+
+namespace Blog {
+	export interface IPost {
+	  title: string;
+	  body: string;
+	}
+
+	export class Post implements IPost {
+	  title: string;
+	  body: string;
+
+	  constructor(post: IPost) {
+	    this.title = post.title;
+	    this.body = post.body;
+	  }
+
+	  printPost() {
+	  	console.log(this.title);
+	  	console.log(this.body);
+	  }
+	}
+}
+
+namespace Content {
+	export interface IPost {
+	  title: string;
+	  body: string;
+	  slug: string;
+	  seoKeywords: string;
+	}
+
+	export class Post implements IPost {
+	  title: string;
+	  body: string;
+	  slug: string;
+	  seoKeywords: string;
+
+	  constructor(post: IPost) {
+	    this.title = post.title;
+	    this.body = post.body;
+	    this.slug = post.slug;
+	    this.seoKeywords = post.seoKeywords;
+	  }
+
+	  printPost() {
+	  	console.log(this.title);
+	  	console.log(this.body);
+	  	console.log(this.slug);
+	  	console.log(this.seoKeywords);
+	  }
+	}
+}
+
+
+var blogPost = new Blog.Post({
+	title: "My Great Post",
+	body: "Some content"
+});
+
+blogPost.printPost();
+
+var contentPost = new Content.Post({
+	title: "My Great Post",
+	body: "Some content",
+	slug: 'my-great-post',
+	seoKeywords: 'any, words'
+});
+
+contentPost.printPost();
+
+
+
+
+
+
+
+
+
